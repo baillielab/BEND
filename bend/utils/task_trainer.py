78cfe7dab2c2f65167ca6e4667cecf361ab0df63
DavideGrecoGit <dg.davidegreco@gmail.com>
2025-05-06 11:36:03 +0100

    fix: allow to run tests with 1 sample

--- bend/utils/task_trainer.py
+++ bend/utils/task_trainer.py
@@ -534,7 +534,9 @@ class BaseTrainer:
         print('TESTING')
         if checkpoint is None:
             df = pd.read_csv(f'{self.config.output_dir}/losses.csv')
-            checkpoint = pd.DataFrame(df.iloc[df[f"val_{self.config.params.metric}"].idxmax()]).T.reset_index(drop=True) 
+            idx = df[f"val_{self.config.params.metric}"].idxmax()
+            idx = idx if not pd.isna(idx) else 0
+            checkpoint = pd.DataFrame(df.iloc[idx]).T.reset_index(drop=True) 
         #print('before load checkpoint', )
         #print(self.model.state_dict()['conv2.1.bias'])
         # load checkpoint
