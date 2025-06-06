24f5e59db97e23c346e4db9ace67c80e184fea2f
DavideGrecoGit <dg.davidegreco@gmail.com>
2025-06-06 17:53:28 +0100

    feat: implement MemoryLess trainer for on the fly training

---
+++ bend/utils/dataloaders.py
@@ -0,0 +1,28 @@
+from torch.utils.data import DataLoader
+from bend.utils.datasets import DatasetMultiHot
+
+
+def get_cpg_dataloader(
+    annotations_path, reference_path, label_depth, batch_size=32, num_workers=0
+):
+    dataloaders = []
+
+    for split in ["train", "valid", "test"]:
+
+        dataset = DatasetMultiHot(
+            annotations_path=annotations_path,
+            genome_path=reference_path,
+            label_depth=label_depth,
+            split=split,
+        )
+
+        dataloaders.append(
+            DataLoader(
+                dataset,
+                batch_size=batch_size,
+                shuffle=True if split == "train" else False,
+                num_workers=num_workers,
+            )
+        )
+
+    return dataloaders
