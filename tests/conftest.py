0a7e3db0b79524769cfc71b1e3acd7bdc9ad2a23
DavideGrecoGit <dg.davidegreco@gmail.com>
2025-06-10 13:36:01 +0100

    feat: share fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,67 @@
+from tqdm.auto import tqdm
+import pandas as pd
+import numpy as np
+from bend.io.sequtils import data_from_bed
+import pytest
+from bend.utils.datasets import DatasetMultiHot
+import os
+from hydra import compose, initialize
+
+
+TASKS = ["cpg_methylation", "histone_modification", "chromatin_accessibility"]
+SPLITS = ["train", "valid", "test"]
+
+with initialize(version_base=None, config_path="../conf/embedding/"):
+    CFG = compose(config_name="embed")
+
+
+def get_gt_data(task, split):
+    chunk_size = CFG["chunk_size"]
+    df = pd.read_csv(CFG[task]["bed"], sep="\t", low_memory=False)
+    df = df[df.iloc[:, -1] == split] if split is not None else df
+    chunks = list(range(int(len(df) / chunk_size) + 1))
+    print(f"Splitting {len(df)} rows into {len(chunks)} chunks of size {chunk_size}.")
+
+    data = {}
+
+    for n, chunk in enumerate(chunks):
+        sequences, labels = data_from_bed(
+            CFG[task]["bed"],
+            CFG[task]["reference_fasta"],
+            label_depth=CFG[task]["label_depth"],
+            read_strand=CFG[task]["read_strand"],
+            split=split,
+            chunk_size=chunk_size,
+            chunk=chunk,
+        )
+
+        data[f"chunk_{chunk}"] = (sequences, labels)
+
+    sequences = []
+    labels = []
+
+    for chunk, (seqs, lbls) in tqdm(data.items(), desc="Merging chunks"):
+        sequences.extend(seqs)
+        labels.extend(lbls)
+
+    return (sequences, labels)
+
+
+def get_dataset(task, split):
+    dataset = DatasetMultiHot(
+        CFG[task]["bed"],
+        CFG[task]["reference_fasta"],
+        label_depth=CFG[task]["label_depth"],
+        split=split,
+    )
+
+    return dataset
+
+
+@pytest.fixture(
+    params=[(task, split) for task in TASKS for split in SPLITS], scope="session"
+)
+def data(request):
+    task, split = request.param
+
+    return get_gt_data(task, split), get_dataset(task, split)
--- tests/test_dataset_multihot.py
+++ tests/test_dataset_multihot.py
@@ -1,97 +1,12 @@
-from tqdm.auto import tqdm
-import pysam
-import pandas as pd
 import numpy as np
-from bend.io.sequtils import data_from_bed
 import pytest
-from bend.utils.datasets import DatasetMultiHot
-import os
-from hydra import compose, initialize
-from omegaconf import OmegaConf
 
 
-with initialize(version_base=None, config_path="../conf/embedding/"):
-    cfg = compose(config_name="embed")
-    # cfg = OmegaConf.to_yaml(cfg)
-    # print(cfg)
+def test_sequences_and_labels(data):
 
-
-def get_annotation_path(task):
-    return os.path.join("data", task, f"{task}.bed")
-
-
-def get_gt_data(task, split):
-    # task, split = request.param
-
-    chunk_size = cfg["chunk_size"]
-    df = pd.read_csv(cfg[task]["bed"], sep="\t", low_memory=False)
-    df = df[df.iloc[:, -1] == split] if split is not None else df
-    chunks = list(range(int(len(df) / chunk_size) + 1))
-    print(f"Splitting {len(df)} rows into {len(chunks)} chunks of size {chunk_size}.")
-
-    data = {}
-
-    for n, chunk in enumerate(chunks):
-        sequences, labels = data_from_bed(
-            cfg[task]["bed"],
-            cfg[task]["reference_fasta"],
-            label_depth=cfg[task]["label_depth"],
-            read_strand=cfg[task]["read_strand"],
-            split=split,
-            chunk_size=chunk_size,
-            chunk=chunk,
-        )
-
-        data[f"chunk_{chunk}"] = (sequences, labels)
-
-    sequences = []
-    labels = []
-
-    for chunk, (seqs, lbls) in tqdm(data.items(), desc="Merging chunks"):
-        sequences.extend(seqs)
-        labels.extend(lbls)
-
-    return sequences, labels
-
-
-def get_dataset_data(task, split):
-    dataset = DatasetMultiHot(
-        cfg[task]["bed"],
-        cfg[task]["reference_fasta"],
-        label_depth=cfg[task]["label_depth"],
-        split=split,
-    )
-
-    return dataset.sequences, dataset.labels
-
-
-@pytest.fixture
-def data(request):
-    task, split = request.param
-
-    gt_seq, gt_labels = get_gt_data(task, split)
-    dataset_seq, dataset_labels = get_dataset_data(task, split)
-
-    return gt_seq, gt_labels, dataset_seq, dataset_labels
-
-
-@pytest.mark.parametrize(
-    "data",
-    [
-        ("cpg_methylation", "train"),
-        ("cpg_methylation", "valid"),
-        ("cpg_methylation", "test"),
-        ("histone_modification", "train"),
-        ("histone_modification", "valid"),
-        ("histone_modification", "test"),
-        ("chromatin_accessibility", "train"),
-        ("chromatin_accessibility", "valid"),
-        ("chromatin_accessibility", "test"),
-    ],
-    indirect=True,
-)
-def test_sequence_labels(data):
-    gt_sequences, gt_labels, dataset_sequences, dataset_labels = data
+    gt_data, dataset = data
+    gt_sequences, gt_labels = gt_data
+    dataset_sequences, dataset_labels = dataset.sequences, dataset.labels
 
     assert len(gt_sequences) == len(dataset_sequences)
     assert len(gt_labels) == len(dataset_labels)
