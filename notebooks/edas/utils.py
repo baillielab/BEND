fa40eec4f179956b18bd906941e1dbfcaf651312
DavideGrecoGit <dg.davidegreco@gmail.com>
2025-07-11 14:37:14 +0100

    feat: short EDA chromatin_accessibility

--- notebooks/edas/utils.py
+++ notebooks/edas/utils.py
@@ -3,10 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def plot_chr_in_splits(annotations):
+def plot_chr_in_splits(annotations, chromosomes):
     sns.set_theme(style="white")
 
-    chromosomes = [f"chr{idx}" for idx in range(1, 23)] + ["chrX", "chrY"]
     annotations["chromosome"] = pd.Categorical(
         annotations["chromosome"], categories=chromosomes, ordered=True
     )
