ea26f5b0e21d953ebc202f7b5ff04a603c1110f9
DavideGrecoGit <dg.davidegreco@gmail.com>
2025-07-28 10:40:22 +0100

    feat: update run_enhancer

--- scripts/run_enhancer.sh
+++ scripts/run_enhancer.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 # This script is used to run the training on the enhancer annotation task with a given embedder.
 
-for fold in 0 1 2 3 4 5 6 7 8 9 
+for fold in 0 1 2 3 4 5 6 7 8 9
 do
-    python scripts/train_on_task.py --config-name=enhancer_annotation embedder=$1 data.cross_validation=$fold shuffle=$2
+    python scripts/train_on_task.py work_dir=$1 data.data_dir=$2 embedder=$3 data.cross_validation=$fold shuffle=$4 data.num_workers=$5 data.batch_size=$6 params.load_checkpoints=$7 --config-name=enhancer_annotation
 done
