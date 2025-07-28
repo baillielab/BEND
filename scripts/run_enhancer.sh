223e10edd57d546f39a9feed676d56e31a77ba12
DavideGrecoGit <dg.davidegreco@gmail.com>
2025-07-28 10:54:05 +0100

    fix: update run_enhancer

--- scripts/run_enhancer.sh
+++ scripts/run_enhancer.sh
@@ -3,5 +3,5 @@
 
 for fold in 0 1 2 3 4 5 6 7 8 9
 do
-    python scripts/train_on_task.py work_dir=$1 data.data_dir=$2 embedder=$3 data.cross_validation=$fold shuffle=$4 data.num_workers=$5 data.batch_size=$6 params.load_checkpoints=$7 --config-name=enhancer_annotation
+    python3 scripts/train_on_task.py work_dir=$1 data.data_dir=$2 embedder=$3 data.cross_validation=$fold shuffle=$4 data.num_workers=$5 data.batch_size=$6 params.load_checkpoints=$7 --config-name=enhancer_annotation
 done
