4df8e51ca12be3dc800a02af10a232d67b9bdb90
DavideGrecoGit <dg.davidegreco@gmail.com>
2025-08-08 11:24:02 +0100

    refact: run undersample experiment after computing all embeddings

--- scripts_hybrid/run_experiment_undersample.sh
+++ scripts_hybrid/run_experiment_undersample.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 # This script runs a series of supervised tasks using specified models and datasets.
 # Usage: ./run_undesample_experiment.sh <task_name> <model_name> <data_dir> <embedders_dir> <embeddings_output_dir (root)>  <output_dir (root)>
-
-
-n_samples=500_000
-echo "Running task: $1 with model: $2 using $n_samples samples"
-python3 scripts_hybrid/experiment_undersample.py \
-    tasks@task=$1 \
-    embedder=$2 \
-    data_dir=$3 \
-    embedders_dir=$4 \
-    embeddings_output_dir=$5/$1/$2 \
-    output_dir=$6/$1/$2/$n_samples \
-    task.dataset.num_train_annotations=$n_samples \
-    task.data.num_train_embeddings=$n_samples \
-    compute_embeddings=true
     
-for n_samples in 10_000 100_000 250_000; 
+for n_samples in 10_000 50_000 100_000 150_000 250_000; 
 do
     echo "Running task: $1 with model: $2 using $n_samples samples"
     python3 scripts_hybrid/experiment_undersample.py \
@@ -25,7 +11,7 @@ do
         data_dir=$3 \
         embedders_dir=$4 \
         embeddings_output_dir=$5/$1/$2 \
-        output_dir=$6/$1/$2/$n_samples \
+        output_dir=$6/$1/$2 $n_samples \
         task.data.num_train_embeddings=$n_samples \
         compute_embeddings=false
     
