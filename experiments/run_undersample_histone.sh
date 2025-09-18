991ecedb319c44111bce01c0d0c4084582d29821
DavideGrecoGit <dg.davidegreco@gmail.com>
2025-09-18 17:33:45 +0100

    feat: add undersample experiments

---
+++ experiments/run_undersample_histone.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# This script runs a series of supervised tasks using specified models and datasets.
+# Usage: . <script_path> <model_name> <data_dir> <embedders_dir> <embeddings_output_dir (root)>  <output_dir (root)> <compute_embeddings>
+
+task=histone_modification
+max_samples=250000
+embedder=$1
+data_dir=$2
+embedders_dir=$3
+embeddings_output_dir=$4
+output_dir=$5
+compute_embeddings=$6
+
+
+if [ $compute_embeddings = "true" ]; then
+    echo "Embedding task: $task with model: $embedder using $max_samples samples"
+    python3 scripts/run_supervised_tasks.py \
+        task=$task \
+        embedder=$embedder \
+        data_dir=$data_dir \
+        embedders_dir=$embedders_dir \
+        embeddings_output_dir=$embeddings_output_dir \
+        output_dir=$output_dir \
+        task.dataset.n_samples=$n_samples \
+        compute_embeddings=true \
+        train_downstream=false
+fi
+
+for n_samples in 10000 50000 100000 150000 250000;
+do
+    echo "Running task: $task with model: $embedder using $n_samples samples"
+    python3 scripts/run_supervised_tasks.py \
+        task=$task \
+        embedder=$embedder \
+        data_dir=$data_dir \
+        embedders_dir=$embedders_dir \
+        embeddings_output_dir=$embeddings_output_dir \
+        output_dir=$output_dir/$n_samples \
+        task.dataset.n_samples=$n_samples \
+        compute_embeddings=false \
+        train_downstream=true
+done
